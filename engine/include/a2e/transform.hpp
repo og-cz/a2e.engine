9e451fc76bfc9221b78a1b434657bacf1b8bccec
og-cz <16796127+og-cz@users.noreply.github.com>
2026-09-12 01:37:18 +0800

    Add transform interface

---
+++ engine/include/a2e/transform.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace a2e {
+
+struct Transform {
+    double x = 0.0;
+    double y = 0.0;
+    double rotation = 0.0;
+    double scale_x = 1.0;
+    double scale_y = 1.0;
+};
+
+} // namespace a2e
